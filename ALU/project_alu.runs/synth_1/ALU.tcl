d416504fc7e3f27516912ca90af1e20de3e72203
PRADEEP-lab-tasks <loganathanp730@gmail.com>
2026-06-26 22:28:41 +0530

    Update synthesis script for ALU project

--- ALU/project_alu.runs/synth_1/ALU.tcl
+++ ALU/project_alu.runs/synth_1/ALU.tcl
@@ -77,15 +77,15 @@ create_project -in_memory -part xc7a35tcpg236-1
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir D:/Aaalalm/analog/project_alu/project_alu.cache/wt [current_project]
-set_property parent.project_path D:/Aaalalm/analog/project_alu/project_alu.xpr [current_project]
+set_property webtalk.parent_dir alu/project_alu.cache/wt [current_project]
+set_property parent.project_path alu/project_alu.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property ip_output_repo d:/Aaalalm/analog/project_alu/project_alu.cache/ip [current_project]
+set_property ip_output_repo alu/project_alu.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
-read_verilog -library xil_defaultlib D:/Aaalalm/analog/project_alu/project_alu.srcs/sources_1/new/ALU.v
+read_verilog -library xil_defaultlib alu/project_alu.srcs/sources_1/new/ALU.v
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
