b5ef47f12dd912923910715ed0297a1942a0f77d
PRADEEP-lab-tasks <loganathanp730@gmail.com>
2026-07-18 00:00:29 +0530

    Update ISEWrap.sh

--- De_Mux/BEHAVIORAL MODELLING(case...)/project_1.runs/impl_1/ISEWrap.sh
+++ De_Mux/BEHAVIORAL MODELLING(case...)/project_1.runs/impl_1/ISEWrap.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-#
-#  Vivado(TM)
-#  ISEWrap.sh: Vivado Runs Script for UNIX
-#  Copyright 1986-2022 Xilinx, Inc. All Rights Reserved. 
-#  Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved. 
-#
+
 
 cmd_exists()
 {
