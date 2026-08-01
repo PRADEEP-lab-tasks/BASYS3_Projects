6b92e571ce8640995552801ca29b6bd8bd583998
PRADEEP-lab-tasks <loganathanp730@gmail.com>
2026-08-01 23:46:34 +0530

    Update oled_test.v

--- OLED/project_Oled.srcs/sources_1/new/oled_test.v
+++ OLED/project_Oled.srcs/sources_1/new/oled_test.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: PRADEEP
 // 
 // Create Date: 27.07.2025 21:33:22
 // Design Name: 
@@ -10,9 +10,7 @@
 // Target Devices: 
 // Tool Versions: 
 // Description: 
-// 
-// Dependencies: 
-// 
+
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
